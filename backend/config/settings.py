cb5eeaa7a4f886126829cb27ec8d3de16db47709
unknown <armin.tree@gmail.com>
2023-03-31 01:34:51 +0430

    015/ jwt

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -144,11 +144,18 @@ REST_FRAMEWORK = {
         'api.permissions.IsStaffOrReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ]
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
+    ],
 }
 
 
 SITE_ID = 1
+
+REST_AUTH = {
+    'USE_JWT': True,
+    'JWT_AUTH_COOKIE': 'access',
+    'JWT_AUTH_REFRESH_COOKIE': 'refresh',
+}
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
